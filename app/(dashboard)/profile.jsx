76747fffaedda27c830b62d787d2223f663aaf89
Acha-Goodness <acharious1668@gmail.com>
2025-12-31 18:42:16 -0800

    logout function integration

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AntDesign } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 
 const Profile = () => {
+
+  const logout = async () => {
+     await AsyncStorage.removeItem("access_token");
+     router.push("/")
+  }
+
   return (
-      <SafeAreaView style={styles.container}>
-      <Text>profile</Text>
+    <SafeAreaView style={styles.container}>
+      <Pressable onPress={logout}>
+        <AntDesign name="poweroff" size={150} color="#f00e12"/>
+      </Pressable>
     </SafeAreaView>
   )
 }
